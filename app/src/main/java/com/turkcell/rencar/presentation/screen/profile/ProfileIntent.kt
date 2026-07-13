69be250c735a47793194ac83cf506f045bf5a2a3
mustafacaglarr <mustafa.caglar147@gmail.com>
2026-07-13 15:30:13 +0300

    feat(profile): show uploaded license photos

--- app/src/main/java/com/turkcell/rencar/presentation/screen/profile/ProfileIntent.kt
+++ app/src/main/java/com/turkcell/rencar/presentation/screen/profile/ProfileIntent.kt
@@ -5,6 +5,8 @@ import com.turkcell.rencar.presentation.core.mvi.UiIntent
 sealed interface ProfileIntent : UiIntent {
     data object ScreenStarted : ProfileIntent
     data object RetryClicked : ProfileIntent
+    data object LicenseStatusClicked : ProfileIntent
+    data object LicensePreviewDismissed : ProfileIntent
     data object LogoutClicked : ProfileIntent
     data object LogoutConfirmed : ProfileIntent
     data object LogoutConfirmationDismissed : ProfileIntent
